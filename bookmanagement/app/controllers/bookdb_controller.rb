02a5c8e52ca15d421c50da2344e3a2f486443718
gjyothi <jyothijgutta@gmail.com>
2023-01-16 02:50:18 +0530

    BookAPI

---
+++ bookmanagement/app/controllers/bookdb_controller.rb
@@ -0,0 +1,48 @@
+class BookdbController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        render json: Bookdb.all
+    end
+
+    def show
+        b = Bookdb.find(params[:id])
+        render json: b
+    rescue => e
+        render json: "Book not found. Kindly input correct data."
+    end
+
+    def create    
+        b= Bookdb.create('bookName': params[:bookName],
+            'bookAuthor': params[:bookAuthor], 
+            'bookQuantity': params[:bookQuantity])  
+        if(b.errors.any?)               
+            render json: "Book already exists, Kindly update quantity"    
+        else 
+            render json: "Book Details Saved!"   
+        end 
+    end
+
+
+
+    def update
+        b=Bookdb.find(params[:id].to_i)
+        b.update('bookName': params[:bookName],
+            'bookAuthor': params[:bookAuthor], 
+            'bookQuantity': params[:bookQuantity])
+        render json: "Book Data Updated Sucessfully!"
+
+        rescue => e
+        render json: "Book not found. Kindly input correct data."
+    
+    end
+
+    def destroy
+        b=Bookdb.find(params[:id].to_i)
+        b.destroy
+        render json: "Book data has been deleted!"
+    rescue => e
+        render json: "Book not found. Kindly input correct data."
+    end
+
+end
